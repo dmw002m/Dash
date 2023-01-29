24f1673bae8fc20831c2c673a61e66170558bcd9
dmw002m <123673672+dmw002m@users.noreply.github.com>
2023-01-29 15:32:45 -0600

    Update rect.py

--- rect.py
+++ rect.py
@@ -33,9 +33,8 @@ def set_color(color_var):
         return "#00ff00"
     elif color_var == "blue":
         return "#0000ff"
-html_code = "<div style='position:relative;'>"
+html_code = "<div style='position:absolute;'>"
     
 st.markdown(f"<div style='width:200px; height:200px; background-color: {color}; border-radius: 20px; margin:10px;  top:10px; left:60px;'></div>", unsafe_allow_html=True)
 
-html_code += "</div>"
-st.markdown(html_code, unsafe_allow_html=True)
+st.markdown(</div>, unsafe_allow_html=True)
