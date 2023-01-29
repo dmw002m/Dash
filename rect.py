fc392adc0a99b777c10c0ee401abf91aa3ffe8f5
dmw002m <123673672+dmw002m@users.noreply.github.com>
2023-01-29 15:07:42 -0600

    Update rect.py

--- rect.py
+++ rect.py
@@ -41,7 +41,7 @@ for i in range(3):
     h = 200
     r = 20
     color = set_color(color_var)
-    st.markdown(f"<div style='width:{w}px; height:{h}px; background-color: {color}; border-radius: {r}px; margin:10px; display:block;'></div>", unsafe_allow_html=True)
+    st.markdown(f"<div style='width:{w}px; height:{h}px; background-color: {color}; border-radius: {r}px; margin:10px; display:inline;'></div>", unsafe_allow_html=True)
 
 st.markdown("</div>", unsafe_allow_html=True)
 
