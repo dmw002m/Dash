371809359690bfdeedcbcb2fdd4f26b317b127d5
dmw002m <123673672+dmw002m@users.noreply.github.com>
2023-01-29 15:03:56 -0600

    Update rect.py

--- rect.py
+++ rect.py
@@ -37,14 +37,11 @@ def set_color(color_var):
 st.markdown("<div style='display: flex; flex-wrap: nowrap;'>", unsafe_allow_html=True)
 
 for i in range(3):
-    x = 50
-    y = 50*i+10
     w = 40
     h = 40
-    r = 2
+    r = 20
     color = set_color(color_var)
     st.markdown(f"<div style='width:{w}px; height:{h}px; background-color: {color}; border-radius: {r}px; margin:10px; display:inline-block;'></div>", unsafe_allow_html=True)
-
 
 st.markdown("</div>", unsafe_allow_html=True)
 
